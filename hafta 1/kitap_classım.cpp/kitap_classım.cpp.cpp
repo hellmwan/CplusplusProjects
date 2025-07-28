9096d10a7bae0e23542162865e23a69a7ac1a731
hellmwan <160488017+hellmwan@users.noreply.github.com>
2025-07-28 17:50:07 +0300

    Add files via upload
    Gereksiz dosyalardan .gitignore yardımıyla kurtulduk, ilk gün programı sonucunda book classı örneği ile, class konusunda geliştirme ve pekiştirme sağladım. Destructor, constructor, this pointerı gibi terimleri pekiştirdim.

---
+++ hafta 1/kitap_classım.cpp/kitap_classım.cpp.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+using namespace std;
+class book {
+private:
+	string name;
+	string author;
+	int page_number;
+
+public:
+	book(string name, string author, int page_number) { // constructer, used this pointer
+		this->name = name;
+		this->author = author;
+		this->page_number = page_number;
+	}
+
+	void printBookInfo() {
+		cout << "book's name is " << name << endl;
+		cout << "book's author is " << author << endl;
+		cout << "book's page number is " << page_number << endl;
+	}
+
+	~book() { // destructer
+		cout << "object is deleted." << endl;
+	}
+
+};
+int main() {
+	book x("Kumarbaz", "Fyodor Mihailovic Dostoyevski", 235);
+	x.printBookInfo();
+	return 0;
+
+}
